a111d158a09d23b402f54155fde8f154b3bfad91
samin <samincgs@gmail.com>
2024-06-05 23:52:09 -0400

    users npw get redirected to the home page if the id for the property is wrong

--- app/(dashboard)/properties/[id]/page.tsx
+++ app/(dashboard)/properties/[id]/page.tsx
@@ -1,4 +1,14 @@
-const page = () => {
+import { fetchPropertyDetails } from '@/utils/actions';
+import { redirect } from 'next/navigation';
+const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) {
+    redirect('/');
+  }
+
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+
   return <div>page</div>;
 };
-export default page;
+export default PropertyDetailsPage;
