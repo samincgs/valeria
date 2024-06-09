e7af62cac9f51e0da7cda74c5ce28725e86f9ee7
samin <samincgs@gmail.com>
2024-06-09 01:02:11 -0400

    added rating and comment to review card

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,4 +1,30 @@
+'use client';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
 const Comment = ({ comment }: { comment: string }) => {
-  return <div className=''>Comment</div>;
+  const [isExpanded, setIsExpanded] = useState(false);
+  const isLongComment = comment.length > 200;
+  const displayedComment =
+    isLongComment && !isExpanded ? `${comment.slice(0, 200)}...` : comment;
+
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  return (
+    <div className=''>
+      <p className='text-sm leading-relaxed'>{displayedComment}</p>
+      {isLongComment && (
+        <Button
+          variant={'link'}
+          className='pl-0 capitalize'
+          onClick={toggleExpanded}
+        >
+          {isExpanded ? 'hide' : 'show more'}
+        </Button>
+      )}
+    </div>
+  );
 };
 export default Comment;
